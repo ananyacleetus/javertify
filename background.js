41402640f864cacb14ddffe0e62100f83d22bb9e
Ananya W Cleetus <ananyacleetus@gmail.com>
2015-03-22 23:29:37 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,35 +2,7 @@
     var self = $.javert = new function(){};
     $.extend(self, {
         javertImgs : [
-			"http://i.imgur.com/oWZuIqa.png",
-			"http://i.imgur.com/vIymbNq.png",
-			"http://i.imgur.com/DL4f66M.png",
-			"http://i.imgur.com/0MhTrQx.png",
-			"http://i.imgur.com/rGcORqX.png",
-			"http://i.imgur.com/EY0CJ4a.png",
-			"http://i.imgur.com/XVgcWdP.png",
-			"http://i.imgur.com/QQhtYa2.png",
-			"http://i.imgur.com/evuOriK.png",
-			"http://i.imgur.com/Bmhzg2x.png",
-			"http://i.imgur.com/UCGXKoh.png",
-			"http://i.imgur.com/jdhJJuH.png",
-			"http://i.imgur.com/oG5PiSH.png",
-			"http://i.imgur.com/al50bHO.png",
-			"http://i.imgur.com/RBScdbD.png",
-			"http://i.imgur.com/QJMHPDb.png",
-			"http://i.imgur.com/xO2w0xP.png",
-			"http://i.imgur.com/Yb4wp1z.png",
-			"http://i.imgur.com/V9bJ1gK.png",
-			"http://i.imgur.com/AnE1tG5.png",
-			"http://i.imgur.com/o8IUhDB.png",
-			"http://i.imgur.com/qrsLyY2.png",
-			"http://i.imgur.com/QNy8Ol5.png",
-			"http://i.imgur.com/ozxlsZE.png",
-			"http://i.imgur.com/gULs43R.png",
-			"http://i.imgur.com/qnAymTM.png",
-			"http://i.imgur.com/P7wSdSX.png",
-			"http://i.imgur.com/elz6D5E.png",
-			"http://i.imgur.com/8iznV0h.png",
+			"http://i.imgur.com/ubRc02u.gif"
         ],
         handleImages : function (lstImgs, time) {
             $.each($('img'), function(i,item) { 
